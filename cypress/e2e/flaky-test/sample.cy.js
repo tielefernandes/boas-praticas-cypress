c4812ace7e7c59894c7a18afa7d52a814d3262e2
tielefernandes <tielefernandes@gmail.com>
2023-11-24 09:35:25 -0300

    :recycle: refactor: ajustando teste flake

--- cypress/e2e/flaky-test/sample.cy.js
+++ cypress/e2e/flaky-test/sample.cy.js
@@ -2,12 +2,15 @@ import { faker } from '@faker-js/faker'
 
 describe('Flaky tests bad practice', () => {
   beforeEach(() => {
-    cy.visit('https://wlsf82-hacker-stories.web.app')
+    cy.intercept(
+      'GET',
+      '**/search**',
+      { fixture: 'stories'}
+    ).as('getStories')
 
-    cy.contains('p','Loading ...')
-      .should('be.visible')
-    cy.contains('p','Loading ...')
-      .should('not.exist')
+    cy.visit('https://wlsf82-hacker-stories.web.app')
+    
+    cy.wait('@getStories')
   })
 
   Cypress._.times(10, () => {
@@ -16,10 +19,7 @@ describe('Flaky tests bad practice', () => {
         cy.search(faker.random.word())
       })
 
-      cy.contains('p','Loading ...')
-        .should('be.visible')
-      cy.contains('p','Loading ...')
-        .should('not.exist')
+      cy.wait('@getStories')
 
       cy.get('.last-searches button')
         .should('have.length', 5)
