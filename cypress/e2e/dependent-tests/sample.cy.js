6103e55a2c85cb209465bfc57dc4ea2cc9f291cd
tielefernandes <tielefernandes@gmail.com>
2023-11-29 18:00:34 -0300

    :recycle: refactor: tornando testes independentes

--- cypress/e2e/dependent-tests/sample.cy.js
+++ cypress/e2e/dependent-tests/sample.cy.js
@@ -1,37 +1,39 @@
 describe('Dependent tests bad practice', () => {
   beforeEach(() => {
-    cy.visit('http://notes-serverless-app.com')
-
-    cy.get('.navbar-nav a:contains(Login)').click()
+    cy.visit('http://notes-serverless-app.com/login')
 
     cy.get('#email').type(Cypress.env('user_email'))
     cy.get('#password').type(Cypress.env('user_password'), { log: false })
-    cy.get('button[type="submit"]').click()
-    
+    cy.get('.LoaderButton').click()
+
     cy.contains('h1', 'Your Notes').should('be.visible')
   })
 
-  it('creates a note', () => {
+  it('CRUDs a note', () => {
     cy.contains('Create a new note').click()
 
     cy.get('#content').type('My note')
     cy.contains('Create').click()
 
-    cy.get('.list-group').should('contain', 'My note')
-  })
+    cy.get('.list-group').contains('My note')
+      .click()
 
-  it('edits a note', () => {
-    cy.get('.list-group').contains('My note').click()
     cy.get('#content').type(' updated')
     cy.contains('Save').click()
 
-    cy.get('.list-group').should('contain', 'My note updated')
-    cy.get('.list-group:contains(My note updated)').should('be.visible')
-  })
+    cy.get('.list-group')
+      .should('contain', 'My note updated')
+    cy.get('.list-group').contains('My note updated)')
+      .should('be.visible')
+      .click()
 
-  it('deletes a note', () => {
-    cy.get('.list-group').contains('My note updated').click()
     cy.contains('Delete').click()
+
+    // asserts the list has at least one item before asserting the note deletion
+    // this way, we make sure the list of notes has already rendered
+    cy.get('.list-group a')
+      .its('lenght')
+      .should('be.at.least', 1)
 
     cy.get('.list-group:contains(My note updated)').should('not.exist')
   })
